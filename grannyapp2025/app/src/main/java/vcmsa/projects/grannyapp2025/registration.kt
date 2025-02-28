ff642d4169f5efd1132aad86c963dad7e3598e4e
unknown <monte@ekronds.co.za>
2025-02-28 10:28:53 +0200

    Started building out registration for granny app

---
+++ grannyapp2025/app/src/main/java/vcmsa/projects/grannyapp2025/registration.kt
@@ -0,0 +1,27 @@
+package vcmsa.projects.grannyapp2025
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.doOnTextChanged
+
+class registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registration)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var usernameEdit : TextView = findViewById(R.id.edtRegistration_username)
+        var passwordEdit : TextView = findViewById(R.id.edtRegistration_password)
+        var passwordConfirmEdit : TextView = findViewById(R.id.edtRegistration_confirmpassword)
+
+    }
+}
